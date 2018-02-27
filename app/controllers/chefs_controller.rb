7dca8ead08ce9d594237c9ae0b896a1cb212951e
Miguel Aguilera-Croasdaile <miguel.aguilera.croasdaile@gmail.com>
2018-02-27 15:03:34 +0100

    Added 7 CRUD methods for the Chef controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,44 @@
 class ChefsController < ApplicationController
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
+
+  def index #DONE
+    @chefs = Chef.all
+  end
+
+  def show #DONE
+  end
+
+  def new #DONE
+    @chef = Chef.new
+  end
+
+  def create #DONE
+    @chef = Chef.new(chef_params)
+    @chef.save
+    redirect_to chefs_path
+  end
+
+  def edit #DONE
+    @chef = Chef.find(params[:id])
+  end
+
+  def update #DONE
+    @chef.update(chef_params)
+    redirect_to chefs_path
+  end
+
+  def destroy #DONE
+    @chef.destroy
+    redirect_to chefs_path
+  end
+
+  private
+
+  def set_chef
+    @chef = Chef.find(params[:id])
+  end
+
+  def chef_params
+    params.require(:chef).permit(:name, :title, :rating, :description, :price, :categories)
+  end
 end
