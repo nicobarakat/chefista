5aac27a3c23fa493d0bf5cac9854f021751ef4da
Margaux Noel <noelmargaux@hotmail.fr>
2018-02-27 15:31:48 +0100

    complete chef model with validations, create booking model with validations, create bookings controller with the 7 actions

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,2 +1,8 @@
 class Chef < ApplicationRecord
+  has_many :bookings
+  has_many :users, through: :bookings
+  validates :name, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :categories, presence: true
+  validates :price, presence: true
 end
