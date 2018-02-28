55fda232c3fb3a267c1c9fb3cc1eea14becbbc33
Miguel Aguilera-Croasdaile <miguel.aguilera.croasdaile@gmail.com>
2018-02-28 10:25:04 +0100

    Added faker gem for seeding chefs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do |x|
+  Chef.create(name: Faker::Name.name, title: Faker::Name.title, rating: Faker::Number.between(1, 10), price: Faker::Number.between(10, 20))
+end
+
+Chef.create(name: "a", title: "b", rating: Faker::Number.between(1, 10), price: Faker::Number.between(10, 20))
