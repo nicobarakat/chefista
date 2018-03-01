0ebe4fcab15a13afb25c3f0d469cf131492dfb7e
Margaux Noel <noelmargaux@hotmail.fr>
2018-03-01 17:37:04 +0100

    we should have different images in the chef index now + some style review

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,6 +1,6 @@
 class Chef < ApplicationRecord
   geocoded_by :address
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :name, presence: true
   validates :title, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Chef.destroy_all
 50.times do |x|
   chef = Chef.new(name: Faker::Name.name, title: Faker::Name.title, price: Faker::Number.between(10, 20), address: Faker::Address.street_address)
   chef.remote_photo_url = "https://source.unsplash.com/1600x900/?cook"
