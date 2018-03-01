3554085a225b24883c9aab33432f68d2e455096d
Miguel Aguilera-Croasdaile <miguel.aguilera.croasdaile@gmail.com>
2018-03-01 16:43:34 +0100

    Removed faulty rating on Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 50.times do |x|
-  chef = Chef.new(name: Faker::Name.name, title: Faker::Name.title, rating: Faker::Number.between(1, 10), price: Faker::Number.between(10, 20), address: Faker::Address.street_address)
+  chef = Chef.new(name: Faker::Name.name, title: Faker::Name.title, price: Faker::Number.between(10, 20), address: Faker::Address.street_address)
   chef.remote_photo_url = "https://source.unsplash.com/1600x900/?cook"
   chef.save
 end
